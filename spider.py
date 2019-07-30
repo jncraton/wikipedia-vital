3939704bf5a68db88f01392f41cc781047e7179a
Jon Craton <jncraton@gmail.com>
2019-07-30 14:28:54 -0400

    Store content by section

--- spider.py
+++ spider.py
@@ -37,11 +37,14 @@ def get_mainspace_links(page):
 
 class PageCleaner(HTMLParser):
   def __init__(self, valid_links=[]):
-    self.content = "<!DOCTYPE html>"
+    self.sections = ["<!DOCTYPE html>"]
     self.inactive_until = []
     self.valid_links = set(valid_links)
 
     super().__init__()
+
+  def get_content(self):
+    return ''.join(self.sections)
 
   def handle_starttag(self, tag, attrs):
     cls = dict(attrs).get('class','')
@@ -58,12 +61,14 @@ class PageCleaner(HTMLParser):
       return
   
     if not self.inactive_until:
+      self.sections.append("")
+    
       if tag not in ['base','meta','link']:
         keep_attrs = ""
         if cls: keep_attrs += ' class="' + cls +'"'
         if href and href[2:] in self.valid_links: keep_attrs += ' href="' + href[2:] +'"'
         
-        self.content += f"<{tag}{keep_attrs}>"
+        self.sections[-1] += f"<{tag}{keep_attrs}>"
     elif tag == self.inactive_until[-1]:
       del self.inactive_until[-1]
     elif tag not in ['br', 'img', 'hr', 'wbr', 'meta']:
@@ -71,21 +76,21 @@ class PageCleaner(HTMLParser):
 
   def handle_endtag(self, tag):
     if not self.inactive_until:
-      self.content += f"</{tag}>"
+      self.sections[-1] += f"</{tag}>"
     elif '/' + tag == self.inactive_until[-1]:
       del self.inactive_until[-1]
     
   def handle_data(self, data):
     if not self.inactive_until:
-      self.content += data
+      self.sections[-1] += data
 
 def get_local_html(page,valid_links=[]):
   parser = PageCleaner(valid_links=valid_links)
   parser.feed(get_mobile_html(page))
 
-  print(f"{len(parser.content)} bytes for {page}")
+  print(f"{len(parser.get_content())} bytes for {page}")
   
-  return parser.content
+  return parser.get_content()
     
 def save_content(page, valid_links=[]):
   with open(f'articles/{page}', 'w') as f:
